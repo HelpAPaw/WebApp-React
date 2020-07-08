987434647ad192edc2b8a9ed4c4ba45b5990db1e
unknown <nadunr@gmail.com>
2020-07-08 23:48:02 +0100

    bootstrap fixes

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Home = () =>(
     <div>
-        <h2>Our Mission</h2>
+        <h2>Our Mission </h2>
         <p>It’s a debate that’s been going on for more than a decade, with many swearing by either side. But, there is a right answer, and it’s not quite as clear cut as many would hope.</p>
         <p>In this blog post, we’ll teach you everything you need to know about plain text and HTML emails, and help you choose the right format for your email.</p>
         <p>HTML vs Plain Text: What’s the Difference?</p>
