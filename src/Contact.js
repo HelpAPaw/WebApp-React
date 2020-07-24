8ec6224494f463537255fe2a5ce9454a00678972
Savithri <savithri0707@gmail.com>
2020-07-25 00:30:05 +0100

    Formatted src code based on prettier configuration

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-export const Contact = () =>(
-    <div>
-        <h2>Contact us</h2>
-        <p>It’s a debate that’s been going on for more than a decade, with many swearing by either side. But, there is a right answer, and it’s not quite as clear cut as many would hope.</p>
-    </div>
-)
+export const Contact = () => (
+  <div>
+    <h2>Contact us</h2>
+    <p>
+      It’s a debate that’s been going on for more than a decade, with many
+      swearing by either side. But, there is a right answer, and it’s not quite
+      as clear cut as many would hope.
+    </p>
+  </div>
+);
