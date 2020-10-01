02183b101cf5d4f4e7116eb9a0e67ff592d4f1d5
Priyesh patel <priyeshpatel9843516@icloud.com>
2020-10-01 14:57:42 +0100

    nearly done but trouble with logo

--- src/About.js
+++ src/About.js
@@ -1,60 +1,15 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import './About.css';
 
-const About = () => (
-  <div>
-    <h2>About page</h2>
-    <p>
-      It’s a debate that’s been going on for more than a decade, with many
-      swearing by either side. But, there is a right answer, and it’s not quite
-      as clear cut as many would hope.
-    </p>
-    <p>
-      In this blog post, we’ll teach you everything you need to know about plain
-      text and HTML emails, and help you choose the right format for your email.
-    </p>
-    <p>HTML vs Plain Text: What’s the Difference?</p>
-    <p>
-      Let’s start by taking a quick look at the differences between plain text
-      emails and HTML emails.The simplest explanation is that an HTML will
-      contain images and colors while a plain text email contains no images, no
-      graphics, and no formatting.
-    </p>
-    <p>Here is an example plain text email:</p>
-    <p>
-      It’s a debate that’s been going on for more than a decade, with many
-      swearing by either side. But, there is a right answer, and it’s not quite
-      as clear cut as many would hope.
-    </p>
-    <p>
-      In this blog post, we’ll teach you everything you need to know about plain
-      text and HTML emails, and help you choose the right format for your email.
-    </p>
-    <p>HTML vs Plain Text: What’s the Difference?</p>
-    <p>
-      Let’s start by taking a quick look at the differences between plain text
-      emails and HTML emails.The simplest explanation is that an HTML will
-      contain images and colors while a plain text email contains no images, no
-      graphics, and no formatting.
-    </p>
-    <p>Here is an example plain text email:</p>
-    <p>
-      It’s a debate that’s been going on for more than a decade, with many
-      swearing by either side. But, there is a right answer, and it’s not quite
-      as clear cut as many would hope.
-    </p>
-    <p>
-      In this blog post, we’ll teach you everything you need to know about plain
-      text and HTML emails, and help you choose the right format for your email.
-    </p>
-    <p>HTML vs Plain Text: What’s the Difference?</p>
-    <p>
-      Let’s start by taking a quick look at the differences between plain text
-      emails and HTML emails.The simplest explanation is that an HTML will
-      contain images and colors while a plain text email contains no images, no
-      graphics, and no formatting.
-    </p>
-    <p>Here is an example plain text email:</p>
-  </div>
-);
-
+function About() {
+  return (
+    <div className="main">
+      <h1>Help A Paw </h1>
+      <h2> 2.3.0 build(59)</h2>
+      <button type>Contact us</button>
+      <p> &copy;Milen Marinov 2019</p>
+    </div>
+  );
+}
 export default About;
